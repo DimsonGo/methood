95b85eacdc09fe62cf78440d1f2fcfbca7221b11
DimsonGo <i@bolgov-1.ru>
2024-05-21 13:41:41 +0300

    Дз готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkYear(LocalDate.now().getYear());
+        checkYear(2025);
+        deviceOs(0, 2013);
+
+        int days = calculateDistance(56);;
+        if (days > 0){
+            System.out.println("Для доставки потребуется " + days + " дней");
+        } else {
+            System.out.println("Доставки нет");
+        }
+    }
+
+
+    public static void checkYear(int year) {
+        if (year % 4 == 0 && year / 100 != 0 || year % 400 == 0 && year > 1584) {
+            System.out.println(year + " високосный");
+        } else {
+            System.out.println(year + " невисокосный");
+        }
+    }
+
+    public static int calculateDistance(int distance) {
+        if (distance > 100) {
+            return -1;
+        }
+        int days = 1;
+        if (distance > 20){
+            days++;
+        }
+        if (distance > 60){
+            days++;
+        }
+        return days;
+    }
+
+
+    public static void deviceOs(int userOs, int yearOs) {
+        int currentYear = LocalDate.now().getYear();
+        if (userOs == 0 && currentYear > 2015 && yearOs >= 2015 && yearOs <= 2027){
+            System.out.println("Установите обновление на айфон");
+        } else if (userOs == 0 && yearOs <= currentYear){
+            System.out.println("Уставновите облегченное обновление на айфон");
+        } else if (userOs == 1 && currentYear > 2015 && yearOs >= 2015 && yearOs <= 2027 ) {
+            System.out.println("Установите обновление на андройд");
+        } else if (userOs == 1 && yearOs <= currentYear) {
+            System.out.println("Установите облегченное обновление на андройд");
+        }
     }
 }
